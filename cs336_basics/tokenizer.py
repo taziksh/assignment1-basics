dffa8fc5f82ccb32f20b8cee07b2b78226e8c579
Tazik Shahjahan <tshahjah@uwaterloo.ca>
2026-04-12 18:48:09 -0700

    integrate with adapter for run_train_bpe()

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,8 +1,54 @@
 import regex as re, collections, os, sys, time
-from pretokenization_example import find_chunk_boundaries
+from typing import BinaryIO
 from multiprocessing import Pool
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
 
 def merge_vocab(pair, vocab, pairs):
     new_vocab = {}
@@ -57,7 +103,7 @@ def train_bpe(
     special_tokens: list[str],
     **kwargs,
 ):
-    num_processes = int(sys.argv[1]) if len(sys.argv) > 1 else 4
+    num_processes = kwargs.get("num_processes", 4)
     num_merges = vocab_size - 256 - len(special_tokens)
 
     with open(input_path, "rb") as f:
@@ -109,10 +155,10 @@ def train_bpe(
     return final_vocab, merges
 
 if __name__ == "__main__":
-    input_path = "data/bpe_example.txt"
-    # input_path = "data/TinyStoriesV2-GPT4-valid.txt"
+    # input_path = "data/bpe_example.txt"
+    input_path = "data/TinyStoriesV2-GPT4-valid.txt"
     special_tokens = ["<|endoftext|>"]
-    vocab_size = 263
+    vocab_size = 1000
 
     start = time.time()
     train_bpe(input_path=input_path, vocab_size=vocab_size, special_tokens=special_tokens)
